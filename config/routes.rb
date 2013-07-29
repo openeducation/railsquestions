35549301497733ec2e4d6d4ab9d0d422853dee08
Matthew Burket <matthew@assignitapp.com>
2013-07-28 21:17:45 -0500

    Added Clerance specs.
    Signed-off-by: Matthew Burket <matthew@assignitapp.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Railsquestions::Application.routes.draw do
-  root to: 'high_voltage/pages#show', id: 'index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
