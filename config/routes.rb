5ddde0dec49fe07cb894923c9d063e75d89d3c85
Matthew Burket <matthew@assignitapp.com>
2013-07-30 10:52:03 -0500

    Start Question layouts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Railsquestions::Application.routes.draw do
+  resources :questions  
+  
   root to: 'high_voltage/pages#show', id: 'index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
