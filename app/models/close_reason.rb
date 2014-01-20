df99e2241ba2dfc3bac285b48d91fc32cb9b2a24
Matthew Burket <matthew@assignitapp.com>
2014-01-20 16:25:32 -0600

    Remove unused before_destory method

--- app/models/close_reason.rb
+++ app/models/close_reason.rb
@@ -5,11 +5,5 @@ class CloseReason < ActiveRecord::Base
 
 	has_many :questions
 
-	before_destroy { raise "Close reason cannot be removed." }
-
-	def destory_check
-		unless self.questions.nil?
-			raise "This close reason can't be remove since it has been used on least one question."
-		end
-	end
+	before_destroy { raise "Close reasons cannot be removed." }
 end
