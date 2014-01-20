250e1ea9009513fa466b3e2966032e61b01d6a59
Matthew Burket <matthew@assignitapp.com>
2014-01-20 16:41:11 -0600

    Formatting code on close method for question

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,16 +1,18 @@
 class Question < ActiveRecord::Base
   belongs_to :user
-  
+
   has_many :answers
 
   has_many :flaggings, counter_cache: :flag_count
+
   belongs_to :close_reason
 
   def close(reason)
-	self.closed = true	
-	self.closed_at = Time.now.utc
-	self.close_reason = reason
-	self.save!
+    self.closed = true
+    self.closed_at = Time.now.utc
+    self.close_reason = reason
+    self.save!
+  end
 
   def flag_count
     self.flaggings.count
