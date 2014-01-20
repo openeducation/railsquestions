4806ee5587cbd009250762ee1259c129ada1b8a2
Matthew Burket <matthew@assignitapp.com>
2014-01-20 16:33:03 -0600

    Added database index and validations for questions

--- db/migrate/20130730001914_create_questions.rb
+++ db/migrate/20130730001914_create_questions.rb
@@ -1,13 +1,14 @@
 class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
-      t.string :title
-      t.belongs_to :user, index: true
-      t.text :body
-      t.string :slug
+      t.string :title, null: :false
+      t.belongs_to :user, index: true, null: :false
+      t.text :body, null: :false
+      t.string :slug, null: :false
       t.boolean :closed, default: false, null: :false
       t.belongs_to :close_reason, index: true
       t.datetime :closed_at
+      t.integer :flag_counter
 
       t.timestamps
     end
